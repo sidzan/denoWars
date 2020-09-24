f625045086af8b05e69d4a8493c095cdb4ee2d13
sijan shrestha <sijan@sijans-MacBook-Pro.local>
2020-09-24 13:53:14 +0700

    fix:  naming

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import axiod from "https://deno.land/x/axiod/mod.ts";
 
-const getQuotes =  await axiod.get("http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote")
+const quote =  await axiod.get("http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote")
   .then(({data:{starWarsQuote}}) => starWarsQuote);
 
-export default getQuotes
+export default quote
